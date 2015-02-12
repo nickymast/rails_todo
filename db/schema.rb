608cf7a402c59da880e5de226ed496ae342346d3
Nicole Mast <nicky.mast@gmail.com>
2015-02-12 10:06:42 -0600

    Add scaffold for tasks & migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212144120) do
+ActiveRecord::Schema.define(version: 20150212160506) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.text     "notes"
+    t.date     "due"
+    t.boolean  "done"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "todos", force: true do |t|
     t.string   "title"
